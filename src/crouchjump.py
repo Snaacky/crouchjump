7bd21c9c7da7c7f19f346298ad82c49fff8641a5
Snaacky <Snaacky@users.noreply.github.com>
2017-09-14 19:01:36 -0400

    Added code.

---
+++ src/crouchjump.py
@@ -0,0 +1,18 @@
+import keyboard
+import sys
+from win32gui import GetWindowText, GetForegroundWindow
+
+
+def main():
+    print("{} loaded. You can crouch jump with shift+space.".format(sys.argv[0]))
+
+    while True:
+        if "PLAYERUNKNOWN'S BATTLEGROUNDS" in GetWindowText(GetForegroundWindow()):
+            if keyboard.is_pressed("shift+space"):
+                keyboard.press_and_release("space")
+                keyboard.press_and_release("c")
+                keyboard.press_and_release("c")
+
+
+if __name__ == '__main__':
+    main()
