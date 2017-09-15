53595332306ec348e9f804ad1b4b51c86a272de8
Snaacky <Snaacky@users.noreply.github.com>
2017-09-14 20:59:50 -0400

    Added sleep to stop wasting CPU cycles.

--- src/crouchjump.py
+++ src/crouchjump.py
@@ -1,5 +1,6 @@
 import keyboard
 import sys
+import time
 from win32gui import GetWindowText, GetForegroundWindow
 
 
@@ -7,11 +8,17 @@ def main():
     print("{} loaded. You can crouch jump with shift+space.".format(sys.argv[0]))
 
     while True:
-        if "PLAYERUNKNOWN'S BATTLEGROUNDS" in GetWindowText(GetForegroundWindow()):
-            if keyboard.is_pressed("shift+space"):
-                keyboard.press_and_release("space")
-                keyboard.press_and_release("c")
-                keyboard.press_and_release("c")
+        try:
+            if "PLAYERUNKNOWN'S BATTLEGROUNDS" in GetWindowText(GetForegroundWindow()):
+                if keyboard.is_pressed("shift+space"):
+                    keyboard.press_and_release("space")
+                    keyboard.press_and_release("c")
+                    keyboard.press_and_release("c")
+            time.sleep(0.1)
+        except KeyboardInterrupt:
+            raise
+        except:
+            pass
 
 
 if __name__ == '__main__':
