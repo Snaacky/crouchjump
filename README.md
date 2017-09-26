83ad95ca2bb57af9fecfe6739f76e97e7c0e32f9
Snaacky <Snaacky@users.noreply.github.com>
2017-09-26 17:39:01 -0400

    64-bit, added config options, no longer falsely detected by AV.

--- README.md
+++ README.md
@@ -2,13 +2,10 @@
 A crouch-jump hotkey script for PUBG without having to rely on in-game bugs, autohotkey, and third-party mouse/keyboard drivers.
 
 ## Download
-[You may click here to go to the download page.](https://github.com/Snaacky/Crouchjump/releases/tag/v1.0)
+[You may click here to go to the download page.](https://github.com/Snaacky/Crouchjump/releases/)
 
-## Information
-In the current state, Crouchjump requires you to have space bound to jump and C bound to crouch. If there is demand for it, I will add configuration options to the compiled version so you can set your binds freely.
-
-## Warning
-[poopieQueen](https://twitter.com/poopiequeen), community lead for PUBG, has stated [in her AMA](https://www.reddit.com/r/PUBATTLEGROUNDS/comments/72396a/iama_poopiequeen_ama/dnfg087/) that "at the moment" Bluehole are not banning users for macros so this should be safe to use. If there becomes a time in which it is unsafe, I will update this statement.
+## Legality of Macros
+[poopieQueen](https://twitter.com/poopiequeen), community lead for PUBG, has stated [in her AMA](https://www.reddit.com/r/PUBATTLEGROUNDS/comments/72396a/iama_poopiequeen_ama/dnfg087/) that "at the moment" Bluehole are not banning users for macros so this should be safe to use.
 
 ## Requirements (source code only)
 * [Python 3.x](https://www.python.org/)
--- src/crouchjump.py
+++ src/crouchjump.py
@@ -1,12 +1,55 @@
 import keyboard
+import os
 from win32gui import GetWindowText, GetForegroundWindow
 
-print("Crouchjump.py loaded. Use shift+space to crouch jump.")
 
-while True:
-    if "PLAYERUNKNOWN'S BATTLEGROUNDS" in GetWindowText(GetForegroundWindow()):
-        keyboard.wait("shift+space")
-        keyboard.press_and_release("space")
-        keyboard.press_and_release("space")
-        keyboard.press_and_release("c")
-        keyboard.press_and_release("c")
+def main():
+    if not does_config_exists():
+        print("Config not found. Creating one.")
+        create_config()
+
+    result = read_config()
+    jump = result[0]
+    crouch = result[1]
+    toggle = result[2]
+
+    start_loop(jump, crouch, toggle)
+
+
+def start_loop(jump, crouch, toggle):
+    print("Crouchjump (v1.0.1) loaded. Use {} to crouch jump.".format(toggle))
+
+    while True:
+        if "PLAYERUNKNOWN'S BATTLEGROUNDS" in GetWindowText(GetForegroundWindow()):
+            keyboard.wait(toggle)
+            keyboard.press_and_release(jump)
+            keyboard.press_and_release(jump)
+            keyboard.press_and_release(crouch)
+            keyboard.press_and_release(crouch)
+
+
+def does_config_exists():
+    if os.path.exists(os.getcwd() + "\config.ini"):
+        return True
+
+
+def create_config():
+    with open("config.ini", "a+") as file:
+        file.write("jump=space\n")
+        file.write("crouch=c\n")
+        file.write("toggle=shift+space")
+
+
+def read_config():
+    with open("config.ini", "r") as file:
+        lines = file.readlines()
+        jump = lines[0].replace("jump=", "")
+        jump = jump.replace("\n", "")
+        crouch = lines[1].replace("crouch=", "")
+        crouch = crouch.replace("\n", "")
+        toggle = lines[2].replace("toggle=", "")
+        return jump, crouch, toggle
+
+
+if __name__ == '__main__':
+    main()
