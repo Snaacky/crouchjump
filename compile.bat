1609b813c8b768ab9bdea63fc730491efda386f5
Snaacky <Snaacky@users.noreply.github.com>
2017-09-26 17:39:01 -0400

    Version 1.0.1.
    Moved from 32-bit to 64-bit executable.
    No longer falsely detected by AV.
    Added configuration options.

--- compile.bat
+++ compile.bat
@@ -4,6 +4,6 @@ python %localappdata%\Programs\Python\Python35\Scripts\pyinstaller-script.py src
 @RD /S /Q "build"
 del /s /q "crouchjump.spec"
 cd %~p0/utils/
-verpatch.exe ../dist/crouchjump.exe 1.0.0.0 /va /pv 1.0.0.0 /s description "A crouch-jump hotkey script for PUBG." /s product "Crouchjump" /s copyright "No copyright applied." /s company "https://github.com/snaacky"
+verpatch.exe ../dist/crouchjump.exe 1.0.1.0 /va /pv 1.0.1.0 /s description "A crouch-jump hotkey script for PUBG." /s product "Crouchjump" /s copyright "No copyright applied." /s company "https://github.com/snaacky"
 cd %~p0/src/
 @RD /S /Q "__pycache__"
